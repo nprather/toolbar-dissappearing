cc4478b7e073ce497c4beb183bb28d71719e05e2
Nathan Prather <nathan.prather@gmail.com>
2022-07-07 09:40:06 -0400

    same results

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },SocialSharing
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -24,6 +24,7 @@ export class Tab1Page {
 
   async onSocialShareClicked() {
 
+    this.socialSharing.share(this.bigText);
     this.socialSharing.share('Are We Having Fun Now?');
 
   }
